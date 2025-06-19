9825ce8afe894ca94d654c51bcb352a7df464b5e
Webcart2025 <webcart.team@gmail.com>
2025-06-19 13:14:45 +0530

    title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>OneVasco Visa Service</title>
+  <title>Abizone Visa Service</title>
   <link rel="stylesheet" href="{{ asset('asset/css/homepage.css') }}" />
   
   
